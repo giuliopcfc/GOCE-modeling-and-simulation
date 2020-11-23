15d4ba1ce521c8f0646d981000e8a473fbbb3947
jackvelo <74593755+jackvelo@users.noreply.github.com>
2020-11-23 14:59:09 +0100

    Update ionThruster.m

--- ionThruster.m
+++ ionThruster.m
@@ -1,9 +1,27 @@
 function thrust = ionThruster(xFCV,data)
 
+areaFCV_A0 = data.FCV.A0;
+diameterFCV = sqrt(4*areaFCV_A0/pi);
 
-% areaOrifice = fun(xFCV) (come per valvola distributore ex2)
-% mDot = ...
-% thrust = ...
+% assigning the value to z
+z = (diameterFCV-xFCV)/diameterFCV;
+
+% computation of the orifice area
+alpha = 2*acos(1-2*z);
+areaOrifice = data.D0^2/8*(alpha - sin(alpha));
+
+% since the pressure after the valve is much lower than the critical
+% pressure, the flow is chocked. Hypothesis of homoentropic flow.
+T2 = data.thruster.T2;
+p2 = data.thruster.p2;
+k =  data.thruster.kXe; 
+R = data.const.Ru/data.thruster.mmXe;
+
+mDot = areaOrifice*p2*sqrt(k)/sqrt(T2*R)*(1+0.5*(k-1))^((-k-1)/2*(k-1));
+
+vAcc = sqrt(2*data.thruster.e*data.thruster.deltaV/data.thruster.massIon);
+
+thrust = mDot*vAcc;
 
 
 end
