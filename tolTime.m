e0d887cfc8ef8a08045ea30e6e2d1d759d07c151
jackvelo <74593755+jackvelo@users.noreply.github.com>
2020-12-06 15:21:34 +0100

    Create tolTime.m

---
+++ tolTime.m
@@ -0,0 +1,38 @@
+relTol = 10.^[-14:1:-6];
+absTol = 10.^[-14:1:-6];
+config
+
+% Time span array:
+tspan = [0 data.orbit.period];
+T = zeros(length(absTol), length(relTol));
+
+for i = 1:150
+    i
+    options = odeset('AbsTol',1e-10,'RelTol',1e-8);
+    integrateOdeFun(@odeFun, tspan, data.ode.Y0, options, data);
+end
+
+tspan = [0 5*data.orbit.period];
+for i = 1:length(absTol)
+    i
+    for j = 1:length(relTol)
+        % Integration:
+        options = odeset('AbsTol',absTol(i),'RelTol',relTol(j));
+        tic
+        integrateOdeFun(@odeFun, tspan, data.ode.Y0, options, data);
+        T(i,j) = toc;
+    end
+end
+%%
+figure,
+hold on
+for i = 1:length(absTol)
+    
+    plot(relTol,T(i,:),'linewidth',1.5);
+end
+set(gca,'XScale','log')
+leg = legend('-14','-13','-12','-11','-10','-9','-8','-7','-6',...
+    'Interpreter', 'Latex','NumColumns',2);
+title(leg,'Abs Tol',  'Interpreter', 'Latex')
+grid on
+xlabel('Rel Tol'), ylabel('Time [s]')
